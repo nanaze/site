dc3c60307e1a00e0d86bd8f81a8bf75c7ef22139
Nathan Naze <nanaze@gmail.com>
2020-11-26 10:24:07 -0800

    implement markdown processing

--- articles.py
+++ articles.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from typing import Text
+
 import os
+import subprocess
 
 
 @dataclass
@@ -8,10 +11,19 @@ class Article:
   content: str
 
 
+def _MarkdownToHtml(markdown_str: Text) -> Text:
+  proc = subprocess.run(['markdown'], input=markdown_str, text=True)
+  proc.check_returncode()
+  return proc.stdout
+
+
 def LoadArticles():
   for filename in os.listdir('articles'):
     full_path = os.path.join('articles', filename)
     with open(full_path, 'rt') as f:
       content = f.read()
-      name, _ = os.path.splitext(filename)
-      yield Article(name, f.read())
+      name, ext = os.path.splitext(filename)
+      if ext == '.md':
+        content = _MarkdownToHtml(content)
+
+      yield Article(name, content)
